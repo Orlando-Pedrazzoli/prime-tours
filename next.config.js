a63d32b858ed892855e619e18f6edfe5abcff7bf
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-10-12 21:31:55 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -5,11 +5,6 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'localhost'],
     formats: ['image/avif', 'image/webp'],
   },
-  i18n: {
-    locales: ['pt', 'en', 'es', 'fr'],
-    defaultLocale: 'pt',
-    localeDetection: true,
-  },
 };
 
 module.exports = nextConfig;
